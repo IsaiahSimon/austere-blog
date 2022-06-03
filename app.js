ba5dea5cf8da06d21e5820cc1ae7577053c0ff2a
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-06-02 21:41:23 -0400

    Add feature: Connect to MongoDB Atlas
    - update app.js
    - update list.ejs
    - add mongoose
    - add dotenv

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const DB_PWD = process.env.DB_PWD;
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const startingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -14,46 +17,74 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+mongoose.connect(`mongodb+srv://secret:${DB_PWD}@cluster0.dbfrl.mongodb.net/articlesDB`, { useNewUrlParser: true });
 
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+// Home route
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent, posts: posts });
+
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: startingContent,
+      posts: posts
+    });
+  });
+
 });
 
+// Compose route
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
+
+
+app.get("/posts/:postId", function (req, res) {
+
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+// About route
 app.get("/about", (req, res) => {
   res.render("about", {aboutContent: aboutContent});
 });
 
+// Contact route
 app.get("/contact", (req, res) => {
   res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", (req, res) => {
-  res.render("compose");
-});
+// Listen for requests
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
-});
-
-// Make url for each post
-app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.postName); // also ignore kebab case
-
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title); // also ignore kebab case
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", { title: post.title, content: post.content }); // render post.ejs
-    };
-  });
-});
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(port, () => console.log(`Server started on port ${port}.`));
